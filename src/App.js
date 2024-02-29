8901a6a018717ebfb63f2bf14a6c7bfafb841263
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-02-29 11:49:14 -0700

    updated to V6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Import Routes instead of Route
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriends from './components/AddFriends';
 
-
-
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/">
-          <Login/>
-        </Route>
-
-        <Route exact path="/login">
-          <Redirect to="/"/>
-        </Route>
-
-        <Route exact path="/friends">
-          <FriendsList/>
-        </Route>
-
-        <Route exact path="/friends/add">
-          <AddFriends/>
-        </Route>
-      </div>
-      </Router>
+    <div className="App">
+      {/* Wrap your routes with <Routes> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/friends" element={<FriendsList />} />
+        <Route path="/friends/add" element={<AddFriends />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 const Login = () => {
     const [cred, setCred] = useState({
@@ -9,20 +10,33 @@ const Login = () => {
     const handleChange = (event) => {
         setCred({
             ...cred,
+            [event.target.name]:event.target.value
+        })
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        axios.post('http://localhost:9000/api/login', cred)
+        .then(response =>{
+            console.log(response)
+            localStorage.setItem("token", response.payload);
+        })
+        .catch (error => {
+            console.log(error)
         })
     }
 
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
           <label htmlFor="username">Username:</label>
-          <input onChange={handleChange} id="username"/>
+          <input onChange={handleChange} name="username" id="username"/>
           </div>
           <div>
           <label htmlFor="password">Password:</label>
-          <input onChange={handleChange} type="password" id="password"/>
+          <input onChange={handleChange} name="password" type="password" id="password"/>
           </div>
           <button>Submit</button>
         </form>
